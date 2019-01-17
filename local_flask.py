c52c68d25c174d375ec80fcdfa11802aefcd3d79
Mubarak SULAYMAN <31209490+MubarakSULAYMAN@users.noreply.github.com>
2019-01-17 21:41:24 +0100

    array test

--- local_flask.py
+++ local_flask.py
@@ -21,10 +21,10 @@ def  apis():
 @cross_origin()
 def newsIndex():
 
-    data1 = scrape_legit()
+#     data1 = scrape_legit()
     # data2 = scrape_kwaralefro()
-    data3 = scrape_todayng()
-    data4= scrape_kwaragist()
+#     data3 = scrape_todayng()
+#     data4= scrape_kwaragist()
     # data5 = scrape_theinformant247()
     data6 = scrape_fidelinfo()
 
@@ -33,7 +33,9 @@ def newsIndex():
     
     # data = data1 + data2 + data3 + data4 + data5 + data6 + data7
 
-    data = data1 + data3 + data4 + data6 
+#     data = data1 + data3 + data4 + data6 
+
+    data = data6
 
     response = app.response_class(
         response=json.dumps(data),

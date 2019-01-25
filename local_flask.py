fefd121e25810b1a0a00b8e9238c5221d79ac971
Mubarak SULAYMAN <31209490+MubarakSULAYMAN@users.noreply.github.com>
2019-01-25 10:02:53 +0100

    theInformant247 readAddress issues

--- local_flask.py
+++ local_flask.py
@@ -2,7 +2,7 @@ from flask import Flask, json, jsonify, request
 from flask_cors import CORS, cross_origin
 
 from local_news import (scrape_fidelinfo, scrape_kwaragist, scrape_kwaralefro,
-                        scrape_legit, scrape_theinformant247, scrape_todayng)
+                        scrape_legit, scrape_todayng)
 
 # from local_news import scrape_legit, scrape_kwaralefro, scrape_todayng, scrape_kwaragist, scrape_theinformant247, scrape_fidelinfo, scrape_freshinsight
 
@@ -31,13 +31,12 @@ def newsIndex():
     data2 = scrape_kwaralefro()
     data3 = scrape_todayng()
     data4 = scrape_kwaragist()
-    data5 = scrape_theinformant247()
     data6 = scrape_fidelinfo()
 
     # removed due to old news
     # data8 = scrape_royalfm()
 
-    data = data1 + data2 + data3 + data4 + data5 + data6
+    data = data1 + data2 + data3 + data4 + data6
 
     response = app.response_class(
         response=json.dumps(data),

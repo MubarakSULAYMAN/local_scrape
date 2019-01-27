f9b623f4b749570567ad4adddda219d33bb1702f
Mubarak SULAYMAN <31209490+MubarakSULAYMAN@users.noreply.github.com>
2019-01-27 22:18:37 +0100

    kwaralefro corrected

--- local_flask.py
+++ local_flask.py
@@ -1,7 +1,7 @@
 from flask import Flask, json, jsonify, request
 from flask_cors import CORS, cross_origin
 import local_news
-from local_news import scrape_legit, local_scrape
+from local_news import scrape_legit, scrape_kwaralefro
 # from local_news import scrape_legit, scrape_kwaralefro, scrape_todayng, scrape_kwaragist, scrape_theinformant247, scrape_fidelinfo, scrape_freshinsight
 
 app = Flask(__name__)

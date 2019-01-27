97a3238248c620f10e99cdc17a5673b996087a4e
Mubarak SULAYMAN <31209490+MubarakSULAYMAN@users.noreply.github.com>
2019-01-27 13:11:33 +0100

    Update local_flask.py

--- local_flask.py
+++ local_flask.py
@@ -1,8 +1,8 @@
 from flask import Flask, json, jsonify, request
 from flask_cors import CORS, cross_origin
 
-from local_news import (scrape_fidelinfo, scrape_kwaragist, scrape_kwaralefro,
-                         scrape_todayng)
+from local_news import (scrape_legit, scrape_kwaralefro,
+                        scrape_todayng, scrape_kwaragist, scrape_fidelinfo)
 
 # from local_news import scrape_legit, scrape_kwaralefro, scrape_todayng, scrape_kwaragist, scrape_theinformant247, scrape_fidelinfo, scrape_freshinsight
 
@@ -27,7 +27,7 @@ def apis():
 @cross_origin()
 def newsIndex():
 
-#     data1 = scrape_legit()
+    data1 = scrape_legit()
     data2 = scrape_kwaralefro()
     data3 = scrape_todayng()
     data4 = scrape_kwaragist()
@@ -36,7 +36,7 @@ def newsIndex():
     # removed due to old news
     # data8 = scrape_royalfm()
 
-    data = data2 + data3 + data4 + data6
+    data = data1 + data2 + data3 + data4 + data6
 
     response = app.response_class(
         response=json.dumps(data),
@@ -46,5 +46,5 @@ def newsIndex():
     return response
 
 
-if __name__ == "__main__":
-    app.run(debug=True, port=9000)
+# if __name__ == "__main__":
+#     app.run(debug=True, port=9000)

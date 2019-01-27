fae041fc081cc1f85cf065544e00343020547eaa
Mubarak SULAYMAN <31209490+MubarakSULAYMAN@users.noreply.github.com>
2019-01-27 13:26:37 +0100

    Update local_flask.py

--- local_flask.py
+++ local_flask.py
@@ -1,42 +1,41 @@
 from flask import Flask, json, jsonify, request
-from flask_cors import CORS, cross_origin
-
-from local_news import (scrape_legit, scrape_kwaralefro,
-                        scrape_todayng, scrape_kwaragist, scrape_fidelinfo)
-
+# from flask_cors import CORS, cross_origin
+import local_news
+from local_news import scrape_legit
 # from local_news import scrape_legit, scrape_kwaralefro, scrape_todayng, scrape_kwaragist, scrape_theinformant247, scrape_fidelinfo, scrape_freshinsight
 
 app = Flask(__name__)
-cors = CORS(app)
-CORS(app)
-
+# cors = CORS(app)
+# CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
-@cross_origin()
-def lost():
+# @cross_origin()
+def  lost():
     return "Lost? What are you looking for."
 
-
 @app.route('/api')
-@cross_origin()
-def apis():
+# @cross_origin()
+def  apis():
     return "Nothing here for you..."
 
-
 @app.route('/api/news')
-@cross_origin()
+# @cross_origin()
 def newsIndex():
 
     data1 = scrape_legit()
-    data2 = scrape_kwaralefro()
-    data3 = scrape_todayng()
-    data4 = scrape_kwaragist()
-    data6 = scrape_fidelinfo()
+    # data2 = scrape_kwaralefro()
+    # data3 = scrape_todayng()
+    # data4= scrape_kwaragist()
+    # data5 = scrape_theinformant247()
+    # data6 = scrape_fidelinfo()
+    # data7 = scrape_freshinsight()
 
     # removed due to old news
     # data8 = scrape_royalfm()
-
-    data = data1 + data2 + data3 + data4 + data6
+    
+    data= data1
+    # data= data1 + data2 + data3 + data4 + data5 + data6 + data7 
 
     response = app.response_class(
         response=json.dumps(data),
@@ -46,5 +45,5 @@ def newsIndex():
     return response
 
 
-# if __name__ == "__main__":
-#     app.run(debug=True, port=9000)
+if __name__ == "__main__":
+    app.run(debug = True, port = 9000)
